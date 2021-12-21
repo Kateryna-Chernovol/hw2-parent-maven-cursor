33324d3ace778037b7144bd1a6c2585da192cbdf
Kateryna-Chernovol <schasliva@yahoo.com>
2021-12-22 01:13:44 +0200

    HW2 maven parent commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Fruit apple = new Fruit();
+        apple.setName("apple");
+        apple.setColour("yellow");
+        System.out.println(apple);
+    }
+}
